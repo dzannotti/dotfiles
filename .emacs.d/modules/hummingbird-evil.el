038ab48b424b8bf75b38611693b5d2d252f7e642
dzannotti <d.zannotti@me.com>
2015-12-30 02:59:11 +0000

    updated evil mode keybinds

--- .emacs.d/modules/hummingbird-evil.el
+++ .emacs.d/modules/hummingbird-evil.el
@@ -7,7 +7,12 @@
 ;;; evil-surround
 (require 'evil-surround)
 
+;; make evil use helm
 (define-key evil-ex-map "b" 'helm-buffers-list)
 (define-key evil-ex-map "e" 'find-file)
+
+;; remap $ and ^ to something shiftless
+(define-key evil-normal-state-map "9" 'evil-end-of-line)
+(define-key evil-normal-state-map "8" 'evil-first-non-blank)
 
 (global-evil-surround-mode 1)
